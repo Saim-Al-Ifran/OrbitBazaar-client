839e1706b83e211e1b2aa5b9d30e005cbd560f28
Saim Al Ifran <phpmysql811@gmail.com>
2025-03-27 12:02:33 +0600

    added new arrivals section in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 
 import Banner from "./Sections/Banner/Banner";
 import FeaturedProducts from "./Sections/FeaturedProduct/FeaturedProducts";
+import NewArrivals from "./Sections/NewArrivals/NewArrivals";
 
 const Home = () => {
   return (
        <>
         <Banner/>
         <FeaturedProducts/>
+        <NewArrivals/>
        </>
               
   
