6f619ff8ef6f356a1c3085f171d64f42ff55d858
Saim Al Ifran <phpmysql811@gmail.com>
2025-04-13 19:42:33 +0600

    feat: setup Redux store with RTK and API middleware integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
+ 
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import './styles/globals.css';
 import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { store } from './app/store.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store}>
+       <App />
+    </Provider>
+ 
 )
